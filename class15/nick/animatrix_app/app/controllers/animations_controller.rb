1a977d610e85544639c54fe2cf3a3f23ba2196ed
Nick Harvey <nolds9@yahoo.com>
2015-03-18 18:23:25 -0400

    Adding hw from class 15

---
+++ class15/nick/animatrix_app/app/controllers/animations_controller.rb
@@ -0,0 +1,15 @@
+class AnimationsController < ApplicationController
+
+before_action :authenticate_user!
+  def index
+    if params[:query]
+      query = params[:query].gsub(' ', '+')
+      response = HTTParty.get("http://api.giphy.com/v1/gifs/search?q=#{query}&limit=3&api_key=dc6zaTOxFJmzC")
+      results = JSON.parse(response.body)
+      @results = results['data']
+    else
+      @results = []
+    end
+  end
+  
+end
